a005de2c410233f80955a6c24d3da7136d9e1549
Yann Ashuach <yannashuach@gmail.com>
2018-11-13 16:44:20 +0000

    Update 4_0 to become a trafic lights program

--- 4_0.c
+++ 4_0.c
@@ -5,20 +5,43 @@ typedef volatile unsigned int ioreg;
 #define	PIO_SODR  	(ioreg *) 0xfffff430	// Set Output Data Register
 #define	PIO_CODR  	(ioreg *) 0xfffff434	// Clear Output Data Register
 
-#define RED_LED		0x00000004
-#define AMBER_LED		0x00000003
-#define GREEN_LED		0x00000002
+#define RED_LED		(1 << 2)
+#define AMBER_LED		(1 << 3)
+#define GREEN_LED		(1 << 4)
+#define DELAY 		0x00800000	// Long enough to see a change in LED
 
-void main(void) {
-  *PIO_PER = RED_LED; // Enable control of I/O pin from PIO Controller
+void delay(int);
+
+int main(void)
+{
+	*PIO_PER = RED_LED; // Enable control of I/O pin from PIO Controller
 	*PIO_OER = RED_LED; // Enable output driver for pin
   *PIO_PER = AMBER_LED; // Enable control of I/O pin from PIO Controller
-	*PIO_OER = AMBER_LED; // Enable output driver for pin
+  *PIO_OER = AMBER_LED; // Enable output driver for pin
   *PIO_PER = GREEN_LED; // Enable control of I/O pin from PIO Controller
-	*PIO_OER = GREEN_LED; // Enable output driver for pin
-  while (1) {
-    *PIO_CODR = RED_LED;
-    *PIO_SODR = AMBER_LED;
-    *PIO_SODR = GREEN_LED;
-  }
+  *PIO_OER = GREEN_LED; // Enable output driver for pin
+
+// NOTE: sodr is off codr is on
+	while (1) {
+        *PIO_CODR = RED_LED;
+        *PIO_SODR = AMBER_LED;
+        *PIO_SODR = GREEN_LED;
+	      delay(DELAY);
+	      *PIO_CODR = AMBER_LED;
+	      delay(DELAY);
+        *PIO_SODR = RED_LED;
+        *PIO_SODR = AMBER_LED;
+        *PIO_CODR = GREEN_LED;
+        delay(DELAY);
+        *PIO_CODR = AMBER_LED;
+        *PIO_SODR = GREEN_LED;
+        delay(DELAY);
+	}
+}
+
+void delay(int count)
+{
+	register int i;
+	for (i=count;i>0;i--)
+       		;
 }
